370e6c02e6442a2effc63a65ad6dbffc270d862d
Youri Gruiters <i-youri-i@hotmail.com>
2022-05-11 08:50:21 +0200

    TS onzin toegevoegd

--- voorbeeld-styled-comp/src/app/components/pokemon/Voorbeeld.tsx
+++ voorbeeld-styled-comp/src/app/components/pokemon/Voorbeeld.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface Props {
   voornaam: string;
   achternaam: string;
   leeftijd: number;
+  volgendePagina: () => void;
 }
 
-const Voorbeeld: React.FC<Props> = ({ voornaam, achternaam, leeftijd }) => {
+const Voorbeeld: React.FC<Props> = ({
+  voornaam,
+  achternaam,
+  leeftijd,
+  volgendePagina,
+}) => {
+  const [getal, setGetal] = useState<number | string>(18);
+
+  console.log(getal, setGetal);
+
+  useEffect(() => {
+    setGetal("hoi");
+  }, []);
+
   return (
     <div>
       <h1>{voornaam}</h1>
       <h1>{achternaam}</h1>
       <h1>{leeftijd}</h1>
+      <button onClick={volgendePagina}>Volgende pagina uit VOORBEELD</button>
     </div>
   );
 };
